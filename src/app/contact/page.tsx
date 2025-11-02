d1fed47504757589b3069c2651c017b8f7d03887
Vaibhav Chauhan <vibhuchauhan37@gmail.com>
2025-11-02 22:07:50 +0530

    Basic environment setup

---
+++ src/app/contact/page.tsx
@@ -0,0 +1,14 @@
+const Contact = () =>{
+
+    return(
+        <>
+        <main>You can contact me by : </main>
+        <ul>
+            <li>Mail : @gmail.com</li>
+            <li>Phone : 1231231231</li>
+        </ul>
+        </>
+    )
+}
+
+export default Contact;
